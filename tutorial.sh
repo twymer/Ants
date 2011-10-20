405afd9f4622fff7a6d8e19c3c8facdc780f35b3
Tyler Wymer <twymer@gmail.com>
2011-10-20 13:48:20 -0400

    Added the game runners I've been using

---
+++ tutorial.sh
@@ -0,0 +1,2 @@
+python ../ants_challenge/tools/playgame.py "python3.2 MyBot.py" "python ../ants_challenge/tools/sample_bots/python/GreedyBot.py" --map_file ../ants_challenge/tools/maps/example/tutorial1.map --log_dir ../ants_challenge/game_logs --turns 60 --scenario --food none --player_seed 7 --verbose -e
+
