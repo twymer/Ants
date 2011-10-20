821ac0a85371d6b6cef6277f83706a8ee96af8be
Tyler Wymer <twymer@gmail.com>
2011-10-19 22:54:02 -0400

    Find hills

--- MyBot.py
+++ MyBot.py
@@ -13,7 +13,7 @@ class MyBot:
   # after the bot has received the game settings
   # the ants class is created and setup by the Ants.run method
   def do_setup(self, ants):
-    # initialize data structures after learning the game settings
+    self.hills = []
     self.unseen = []
     for row in range(ants.rows):
       for col in range(ants.cols):
@@ -42,8 +42,9 @@ class MyBot:
         if do_move_direction(loc, direction):
           targets[dest] = loc
           return True
-        else:
-          return False
+          break
+      else:
+        return False
 
     for hill_loc in ants.my_hills():
       orders[hill_loc] = None
@@ -59,12 +60,19 @@ class MyBot:
       if food_loc not in targets and ant_loc not in targets.values():
         do_move_location(ant_loc, food_loc)
 
-    # unblock our hills
-    for hill_loc in ants.my_hills():
-      if hill_loc in ants.my_ants() and hill_loc not in orders.values():
-        for direction in ('s','e','w','n'):
-          if do_move_direction(hill_loc, direction):
-            break
+    # kill hills
+    for hill_loc, hill_owner in ants.enemy_hills():
+      if hill_loc not in self.hills:
+        self.hills.append(hill_loc)
+    ant_dist = []
+    for hill_loc in self.hills:
+      for ant_loc in ants.my_ants():
+        if ant_loc not in orders.values():
+          dist = ants.distance(ant_loc, hill_loc)
+          ant_dist.append((dist, ant_loc))
+    ant_dist.sort()
+    for dist, ant_loc in ant_dist:
+      do_move_location(ant_loc, hill_loc)
 
     # explore map
     for loc in self.unseen[:]:
@@ -79,6 +87,13 @@ class MyBot:
         unseen_dist.sort()
         for dist, unseen_loc in unseen_dist:
           if do_move_location(ant_loc, unseen_loc):
+            break
+
+    # unblock our hills
+    for hill_loc in ants.my_hills():
+      if hill_loc in ants.my_ants() and hill_loc not in orders.values():
+        for direction in ('n','e','s','w'):
+          if do_move_direction(hill_loc, direction):
             break
 
 if __name__ == '__main__':
