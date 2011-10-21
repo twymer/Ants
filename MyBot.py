0a20f146d26159a2f2c5a904351040535c5c5390
Tyler Wymer <twymer@gmail.com>
2011-10-21 08:50:29 -0400

    Pulled out unused ants check

--- MyBot.py
+++ MyBot.py
@@ -13,6 +13,9 @@ class MyBot:
       for col in range(ants.cols):
         self.unseen.append((row, col))
 
+  def inactive_ants(self, ants):
+    return [ant_loc for ant_loc in ants.my_ants() if ant_loc not in self.orders.values()]
+
   def do_move_direction(self, loc, direction, ants):
     new_loc = ants.destination(loc, direction)
     if (ants.unoccupied(new_loc) and new_loc not in self.orders):
@@ -49,10 +52,9 @@ class MyBot:
         self.hills.append(hill_loc)
     ant_dist = []
     for hill_loc in self.hills:
-      for ant_loc in ants.my_ants():
-        if ant_loc not in self.orders.values():
-          dist = ants.distance(ant_loc, hill_loc)
-          ant_dist.append((dist, ant_loc))
+      for ant_loc in self.inactive_ants(ants):
+        dist = ants.distance(ant_loc, hill_loc)
+        ant_dist.append((dist, ant_loc))
     ant_dist.sort()
     for dist, ant_loc in ant_dist:
       self.do_move_location(ant_loc, hill_loc, ants)
@@ -61,16 +63,15 @@ class MyBot:
     for loc in self.unseen[:]:
       if ants.visible(loc):
         self.unseen.remove(loc)
-    for ant_loc in ants.my_ants():
-      if ant_loc not in self.orders.values():
-        unseen_dist = []
-        for unseen_loc in self.unseen:
-          dist = ants.distance(ant_loc, unseen_loc)
-          unseen_dist.append((dist, unseen_loc))
-        unseen_dist.sort()
-        for dist, unseen_loc in unseen_dist:
-          if self.do_move_location(ant_loc, unseen_loc, ants):
-            break
+    for ant_loc in self.inactive_ants(ants):
+      unseen_dist = []
+      for unseen_loc in self.unseen:
+        dist = ants.distance(ant_loc, unseen_loc)
+        unseen_dist.append((dist, unseen_loc))
+      unseen_dist.sort()
+      for dist, unseen_loc in unseen_dist:
+        if self.do_move_location(ant_loc, unseen_loc, ants):
+          break
 
   def unblock_hills(self, ants):
     for hill_loc in ants.my_hills():
