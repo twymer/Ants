182509e25029d77c388c46259e6ad388d8e8cdc1
Tyler Wymer <twymer@gmail.com>
2011-11-01 16:09:24 -0400

    Script to pit my bot against itself

---
+++ self_fight.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+# python ../ants_challenge/tools/playgame.py --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file ../ants_challenge/tools/maps/maze/maze_1.map "$@" "python ../ants_challenge/tools/sample_bots/python/GreedyBot.py" "python3.2 MyBot.py" -So | java -jar ../ants_challenge/tools/visualizer.jar
+python ../ants_challenge/tools/playgame.py --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file ../ants_challenge/tools/maps/multi_hill_maze/multi_maze_02.map "$@" "python3.2 MyBot.py" "python3.2 MyBot.py" --log_dir ../ants_challenge/game_logs -e -So | java -jar ../ants_challenge/tools/visualizer.jar
