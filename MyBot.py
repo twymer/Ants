17a608127cb1b7a596858c2259ce38e87031c463
Tyler Wymer <twymer@gmail.com>
2011-11-14 19:32:04 -0500

    New search code! Monstrous performance boosts. Code rewritten to no longer cache paths. Bot is dumber than before, but is getting rewritten.

--- MyBot.py
+++ MyBot.py
@@ -3,17 +3,103 @@ from random import shuffle
 from ants import *
 from collections import namedtuple
 import logging
+import heapq
 
 from time import time
 
 MY_ANT = 0
 
+# version 0.1.0 -- DIRTY
+class Search:
+  def __init__(self, env):
+    self.environment = env
+    self.N = self.environment.rows
+    self.M = self.environment.cols
+
+  def manhattan_distance(self, start, goal):
+    s0 = start[0]
+    g0 = goal[0]
+    # TODO: Remove this before end of contest
+    # Or investigate using an assert and python -O on server to stop the check
+    if s0 >= self.N or g0 >= self.N:
+      raise ValueError("Row value out of bounds")
+    if s0 > g0:
+      g0, s0 = s0, g0
+
+    s1 = start[1]
+    g1 = goal[1]
+    # TODO: Remove this before end of contest
+    if s1 >= self.M or g1 >= self.M:
+      raise ValueError("Column value out of bounds")
+    if s1 > g1:
+      g1, s1 = s1, g1
+
+    return min((g0 - s0, s0 + self.N - g0)) + min((g1 - s1, s1 + self.M - g1))
+
+  def find_path(self, start_position, goal_position, next_turn_list):
+    if not self.environment.passable(goal_position):
+        return False
+    Node = namedtuple('Node', 'position f g h parent depth')
+    #logging.error("find_path")
+
+    def trace_path(final_node):
+      t = time()
+      path = []
+      current = final_node
+      while current.parent is not None:
+        path.append(current.position)
+        current = current.parent
+
+      # self.tracing_time += time() - t
+      return path
+
+    open_nodes = {}
+    open_nodes_heap = []
+    closed_nodes = {}
+    open_heap = []
+
+    start_h = self.manhattan_distance(start_position, goal_position)
+    current = Node(start_position, start_h, 0, start_h, None, 0)
+    open_nodes[current.position] = current
+    heapq.heappush(open_nodes_heap, (current.f, current))
+    while open_nodes:
+      # Grab item with minimum F value
+      _, current = heapq.heappop(open_nodes_heap)
+      del open_nodes[current.position]
+      #current = min(open_nodes.values(), key=lambda x:x.f)
+      if current.position == goal_position:
+        # logging.error("steps to find path: " + str(current.depth))
+        return trace_path(current)
+      closed_nodes[current.position] = current
+      for neighbor in self.environment.neighbors[current.position]:
+        if (neighbor not in open_nodes and # and not open
+            neighbor not in closed_nodes and # or closed
+            (current.depth > 0 or neighbor not in next_turn_list) and # if occupied and next to start
+            self.environment.passable(neighbor)): # if occupied and next to start
+          new_g = current.g + 1
+          new_h = self.manhattan_distance(neighbor, goal_position)
+          new = Node(
+            neighbor,
+            new_g,
+            new_h,
+            new_g + new_h,
+            current,
+            current.depth + 1)
+          open_nodes[new.position] = new
+          heapq.heappush(open_nodes_heap, (new.f, new))
+      # If first item has no neighbors, ant is trapped, put a noop on his move list
+      if current.depth == 0 and len(open_nodes) == 0:
+        return [current.position]
+
+    # Making it through the loop means we explored all points reachable but
+    # did not find our goal
+    return None
+
 class MyBot:
   # define class level variables, will be remembered between turns
   def __init__(self):
     self.enemy_hills = set()
-    self.move_list = {}
-    self.initialize_global_timing()
+    # self.initialize_global_timing()
 
   def initialize_global_timing(self):
     self.global_pathing_time = 0
@@ -25,22 +111,14 @@ class MyBot:
     self.find_new_time = 0
 
   def do_setup(self, ants):
-    self.aborted_count = 0
     self.hills = []
 
+    # TODO: Store last time a location was seen
+    # so that we don't ever run out of territory to search
     self.unseen = []
     for row in range(ants.rows):
       for col in range(ants.cols):
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        count = 0
-        for d_row, d_col in directions:
-          if not ants.passable((((row+d_row) % ants.rows), (col+d_col) % ants.cols)):
-            count += 1
-
-        # If there is only 1 side available this is a crevice in the
-        # wall that we don't want to consider for exploration
-        if count < 3:
-          self.unseen.append((row, col))
+        self.unseen.append((row, col))
 
   def do_move_direction(self, loc, direction, ants):
     new_loc = ants.destination(loc, direction)
@@ -55,97 +133,10 @@ class MyBot:
     directions = ants.direction(loc, dest)
     for direction in directions:
       if self.do_move_direction(loc, direction, ants):
-        self.targets[dest] = loc
+        # self.targets[dest] = loc
         return True
     else:
       return False
-
-  def find_path(self, start_position, goal_position, ants):
-    Node = namedtuple('Node', 'position f g h parent depth')
-    #logging.error("find_path")
-
-    # TODO: This doesn't consider map wrap around
-    # Source: http://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html
-    def manhattan_distance(start, goal):
-      d = 2 # movement cost
-      return d * (abs(start[0] - goal[0]) + abs(start[1] - goal[1]))
-
-    def neighbors(pos):
-      directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-      return [((pos[0] + d_row) % ants.rows, (pos[1] + d_col) % ants.cols)
-          for (d_row, d_col) in directions]
-
-    def trace_path(final_node):
-      #logging.error("start trace path")
-      t = time()
-      path = []
-      current = final_node
-      while current.parent is not None:
-        path.append(current.position)
-        current = current.parent
-      #logging.error("end trace path")
-
-      self.tracing_time += time() - t
-      return path
-
-    def has_node_with_position(node_list, position):
-      return any(position == node.position for node in node_list)
-
-    #logging.error("passable start? " + str(ants.passable(start)))
-    #logging.error("passable goal? " + str(ants.passable(target)))
-
-    open_set = set()
-    closed_set = set()
-
-    start_h = manhattan_distance(start_position, goal_position)
-    current = Node(start_position, start_h, 0, start_h, None, 0)
-    open_set.add(current)
-    while open_set:
-      #TODO: Timing out and sending the ant to this place isn't very nice
-      # Maybe flag it as a low quality path?
-      if current.depth > max(50, 100 - self.aborted_count * 20):
-        self.aborted_count += 1
-        logging.error("Search aborted early! " + str(current.depth) + " turns.")
-        return trace_path(current)
-
-      # Grab item with minimum F value
-      current = min(open_set, key=lambda x:x.f)
-      #logging.error("current.position " + str(current.position))
-      #logging.error("goal " + str(goal))
-      if current.position == goal_position:
-        #logging.error("start: " + str(start))
-        #logging.error("****")
-        logging.error("steps to find path: " + str(current.depth))
-        #logging.error("****")
-        return trace_path(current)
-      open_set.remove(current)
-      closed_set.add(current)
-      for neighbor in neighbors(current.position):
-        #logging.error("  in open? " + str(has_node(open_list, neighbor)))
-        #logging.error("  in closed? " + str(has_node(closed_list, neighbor)))
-        #logging.error("  is passable? " + str(ants.passable(neighbor)))
-        if (ants.passable(neighbor) and # Add if passable..
-            not has_node_with_position(open_set, neighbor) and # and not open
-            not has_node_with_position(closed_set, neighbor) and # or closed
-            (current.depth > 1 or ants.unoccupied(neighbor))): # if occupied and next to start
-          new_g = current.g + 1
-          new_h = manhattan_distance(neighbor, goal_position)
-          new = Node(
-            neighbor,
-            new_g,
-            new_h,
-            new_g + new_h,
-            current,
-            current.depth + 1)
-          #logging.error("  neighbor " + str(neighbor))
-          #logging.error("  how far in? " + str(current[NUM]))
-          #logging.error("  g? " + str(current[G]))
-          #logging.error("  h? " + str(current[H]))
-          #logging.error("  f=g+h? " + str(current[F]))
-          open_set.add(new)
-    # Making it through the loop means we explored all points reachable but
-    # did not find our goal
-    return None
 
   def find_food(self, ants):
     ant_dist = []
@@ -155,113 +146,111 @@ class MyBot:
         ant_dist.append((dist, ant_loc, food_loc))
     ant_dist.sort()
 
-    #logging.error("ant_dist: " + str(ant_dist))
-
     for dist, ant_loc, food_loc in ant_dist:
-      if food_loc not in self.targets and ant_loc not in self.targets.values() and ant_loc not in self.move_list and not any(food_loc in x for x in self.move_list.values()):
-        logging.error("Find food path from " + str(ant_loc) + " to " + str(food_loc))
-        t = time()
-        path = self.find_path(ant_loc, food_loc, ants)
-        #logging.error("path time: " + str(time() - t))
+      if food_loc not in self.targets and ant_loc not in self.orders:
+        path = self.search.find_path(ant_loc, food_loc, self.next_turn_list)
         if path:
-          #logging.error("path found!")
-          self.move_list[ant_loc] = path
-        #else:
-        #  # Go ahead and add the ant with no path to move list... this usually
-        #  # means a game bug and we don't want to go crazy trying to path him
-        #  # TODO: fix this..
-        #  self.move_list[ant_loc] = []
-
-        #logging.error("***")
+          first_move = path.pop()
+        else:
+          continue
+        logging.error("food from: " + str(ant_loc) + " to " + str(first_move))
+        if first_move not in self.next_turn_list:
+          self.do_move_location(ant_loc, first_move, ants)
+          self.orders[ant_loc] = food_loc
+          self.next_turn_list.add(first_move)
+          self.targets.add(food_loc)
 
   def find_hills(self, ants):
     for hill, owner in self.enemy_hills.copy():
       # If we can see a hill but it's not in the enemy hills list,
       # that means we have destroyed it
-      if ants.visible(hill) and hill not in ants.enemy_hills():
+      logging.error("enemy hills: " + str(ants.enemy_hills()))
+      logging.error("hill: " + str(hill))
+# hill_loc in x for x in self.move_list.values()
+      if ants.visible(hill) and not any(hill == x for x in ants.enemy_hills()[0]):
+        logging.error("*** REMOVE HILL ***")
         self.enemy_hills.remove((hill, owner))
 
     self.enemy_hills = self.enemy_hills.union(ants.enemy_hills())
+
+    # TODO: unchecked
     for hill_loc, hill_owner in self.enemy_hills:
       if hill_loc not in self.hills:
         self.hills.append(hill_loc)
     ant_dist = []
     for hill_loc in self.hills:
       for ant_loc in ants.my_ants():
-        if ant_loc not in self.move_list:
+        if ant_loc not in self.orders:
           dist = ants.distance(ant_loc, hill_loc)
           ant_dist.append((dist, ant_loc, hill_loc))
     ant_dist.sort()
 
+    # self.orders = {}
+    # self.next_turn_list = set()
+    # self.targets = set()
     for dist, ant_loc, hill_loc in ant_dist:
-      if ant_loc not in self.move_list and not any(hill_loc in x for x in self.move_list.values()):
-        logging.error("Find hills path")
-        t = time()
-        path = self.find_path(ant_loc, hill_loc, ants)
-        self.pathing_time += time() - t
-        logging.error("path time: " + str(time() - t))
+      if ant_loc not in self.orders and hill_loc not in self.targets:
+        path = self.search.find_path(ant_loc, hill_loc, self.next_turn_list)
         if path:
-          self.move_list[ant_loc] = path
-        #else:
-        #  # Go ahead and add the ant with no path to move list... this usually
-        #  # means a game bug and we don't want to go crazy trying to path him
-        #  # TODO: fix this..
-        #  self.move_list[ant_loc] = []
+          first_move = path.pop()
+        else:
+          continue
+        logging.error("hill from: " + str(ant_loc) + " to " + str(first_move))
+        if first_move not in self.next_turn_list:
+          self.do_move_location(ant_loc, first_move, ants)
+          self.orders[ant_loc] = hill_loc
+          self.next_turn_list.add(first_move)
+          self.targets.add(hill_loc)
 
   def find_new(self, ants):
-    time_start = time()
-    path_time_start = self.pathing_time
-
+    # Remove currently visible territory
     for loc in self.unseen[:]:
       if ants.visible(loc):
         self.unseen.remove(loc)
 
     for ant_loc in ants.my_ants():
-      if ant_loc in self.move_list:
+      if ant_loc in self.orders:
         continue
 
-      #check
       unseen_dist = []
       for unseen_loc in self.unseen:
-        dist = ants.distance(ant_loc, unseen_loc)
+        if unseen_loc in self.targets:
+          continue
+        dist = self.search.manhattan_distance(ant_loc, unseen_loc)
         unseen_dist.append((dist, unseen_loc))
       unseen_dist.sort()
 
       for dist, unseen_loc in unseen_dist:
-        if self.move_list and ant_loc not in self.move_list and not any(unseen_loc in x for x in self.move_list.values()):
-          logging.error("Find territory path from " + str(ant_loc) + " to " + str(unseen_loc))
-          t = time()
-          path = self.find_path(ant_loc, unseen_loc, ants)
-          self.pathing_time += time() - t
-          logging.error("path time: " + str(time() - t))
+        if unseen_loc not in self.targets and ant_loc not in self.orders:
+          # TODO: this code isn't very DRY
+          path = self.search.find_path(ant_loc, unseen_loc, self.next_turn_list)
           if path:
-            #logging.error("added to move list")
-            self.move_list[ant_loc] = path
-          #else:
-          #  # Go ahead and add the ant with no path to move list... this usually
-          #  # means a game bug and we don't want to go crazy trying to path him
-          #  # TODO: fix this..
-          #  self.move_list[ant_loc] = None
-    self.find_new_time += time() - time_start - self.pathing_time - path_time_start
+            first_move = path.pop()
+          else:
+            continue
+          logging.error("new from: " + str(ant_loc) + " to " + str(first_move))
+          if first_move not in self.next_turn_list:
+            self.do_move_location(ant_loc, first_move, ants)
+            self.orders[ant_loc] = unseen_loc
+            self.next_turn_list.add(first_move)
+            self.targets.add(unseen_loc)
+          else:
+            logging.error("first move taken")
+
 
   def do_turn(self, ants):
-    self.initialize_timing()
-    t = time()
-    self.orders = {}
-    self.targets = {}
-    #logging.error("*****")
-    #logging.error("*****")
-    #logging.error("*****")
     logging.error("*****")
     logging.error("Start turn")
     logging.error("*****")
 
-    self.aborted_count = 0
+    self.initialize_timing()
+    t = time()
 
-    # Mourn our fallen comrades!
-    for dead_ant, owner in ants.dead_list.items():
-      if owner == [MY_ANT] and dead_ant in self.move_list:
-        del self.move_list[dead_ant]
+    self.search = Search(ants)
+
+    self.orders = {}
+    self.next_turn_list = set()
+    self.targets = set()
 
     # Do we need this?
     #for hill_loc in ants.my_hills():
@@ -269,43 +258,23 @@ class MyBot:
     #logging.error("move list start turn" + str(self.move_list))
 
     self.find_food(ants)
-    self.find_new(ants)
     self.find_hills(ants)
+    self.find_new(ants)
 
-    #logging.error("targets: " + str(self.targets))
+    # logging.error("*****")
+    # logging.error("*****")
+    # self.global_pathing_time += self.pathing_time - self.tracing_time
+    # logging.error("turns pathing time: " + str(self.pathing_time - self.tracing_time))
+    # logging.error("global pathing time: " + str(self.global_pathing_time))
 
-    new_move_list = {}
-    for ant_loc, ant_moves in self.move_list.items():
-      if ant_moves:
-        next_move = ant_moves.pop()
-      else:
-        break
-      if self.do_move_location(ant_loc, next_move, ants):
-        # This will become the global move list later
-        # containing only successfully walked paths
-        if len(ant_moves):
-          new_move_list[next_move] = ant_moves
-      else:
-        logging.error("move failed! " + str(ant_loc) + " to " + str(next_move))
-
-    # TODO: If this gets large, will we need to not slam garbage collection
-    # so hard?
-    self.move_list = new_move_list
-
+    # self.global_tracing_time += self.tracing_time
+    # logging.error("turns tracing time: " + str(self.tracing_time))
+    # logging.error("global tracing time: " + str(self.global_tracing_time))
+    # logging.error("non pathing time: " + str(time() - t - self.pathing_time))
+    # logging.error("find new time: " + str(self.find_new_time))
     logging.error("*****")
-    logging.error("*****")
-    self.global_pathing_time += self.pathing_time - self.tracing_time
-    logging.error("turns pathing time: " + str(self.pathing_time - self.tracing_time))
-    logging.error("global pathing time: " + str(self.global_pathing_time))
-
-    self.global_tracing_time += self.tracing_time
-    logging.error("turns tracing time: " + str(self.tracing_time))
-    logging.error("global tracing time: " + str(self.global_tracing_time))
-    logging.error("*****")
-    logging.error("*****")
-    logging.error("non pathing time: " + str(time() - t - self.pathing_time))
-    logging.error("find new time: " + str(self.find_new_time))
     logging.error("End turn")
+    logging.error("*****")
 
 if __name__ == '__main__':
   try:
